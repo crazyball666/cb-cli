dacd1e9f186a509098a12d715b426f94454e0bc0
crazyball <774028406@qq.com>
2018-09-25 21:03:56 +0800

    test

--- bin/cb-cli.js
+++ bin/cb-cli.js
@@ -1,4 +1,24 @@
-const exec = require('child_process').exec;
-exec('mkdir aaa', function () {
-  console.log('king init命令已执行...');
-});
+const init = require('../script/init.js');
+// const start = require('../src/start.js');
+const program = require('commander');
+const pkg = require('../package.json');
+process.title = 'king';
+program
+  .version(pkg.version);
+
+program
+  .command('init [project]')
+  .description('init project')
+  .action(function (project) {
+    init.run(project);
+  });
+
+// program
+//   .command('start')
+//   .description('start project')
+//   .option('-p, --port [port]', 'listening port')
+//   .action(function (port) {
+//     start.run(port);
+//   });
+
+program.parse(process.argv);
