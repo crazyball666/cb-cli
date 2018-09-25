a1c347502f92f766106eda8d03e6c286172b1da6
crazyball <774028406@qq.com>
2018-09-25 21:15:36 +0800

    test

--- bin/cli.js
+++ bin/cli.js
@@ -3,22 +3,23 @@ const start = require('../src/start.js');
 const program = require('commander');
 const pkg = require('../package.json');
 
-program
-  .version(pkg.version);
+console.log(123);
+// program
+//   .version(pkg.version);
 
-program
-  .command('init [project]')
-  .description('init project')
-  .action(function (project) {
-    init.run(project);
-  });
+// program
+//   .command('init [project]')
+//   .description('init project')
+//   .action(function (project) {
+//     init.run(project);
+//   });
 
-program
-  .command('start')
-  .description('start project')
-  .option('-p, --port [port]', 'listening port')
-  .action(function (port) {
-    start.run(port);
-  });
+// program
+//   .command('start')
+//   .description('start project')
+//   .option('-p, --port [port]', 'listening port')
+//   .action(function (port) {
+//     start.run(port);
+//   });
 
-program.parse(process.argv);
+// program.parse(process.argv);
