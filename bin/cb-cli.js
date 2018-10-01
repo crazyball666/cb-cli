9e4ea19fc8188bc4878a44ebed2983d1a7fe48e1
crazyball <774028406@qq.com>
2018-10-01 21:12:07 +0800

    开发模式添加监听端口参数

--- bin/cb-cli.js
+++ bin/cb-cli.js
@@ -1,7 +1,14 @@
 #!/usr/bin/env node
 const dev = require('../src/dev');
 const argv = process.argv[2];
+// webpack-dev-server 默认监听端口
+let port = 8080;
+let portIndex = process.argv.indexOf('--port');
+if (portIndex !== -1) {
+  process.argv[portIndex + 1] && (port = process.argv[portIndex + 1]);
+}
 console.log(argv);
+console.log(port);
 
 if (argv === '-v') {
 
