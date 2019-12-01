8b63c792b002f919a9566222076465abbb962688
crazyball <774028406@qq.com>
2019-12-01 22:59:23 +0800

    完善cb-cli

--- bin/cb-cli.js
+++ bin/cb-cli.js
@@ -1,152 +1,191 @@
 #!/usr/bin/env node
-const webpack = require("webpack");
-const path = require("path");
-const logger = require("../logger");
-const fs = require("fs");
-const MemoryFileSystem = require("memory-fs");
-const memoryFs = new MemoryFileSystem();
-const argv = process.argv[2];
 
-const Koa = require('koa')
-const app = new Koa()
+const webpack = require('webpack');
+const path = require('path');
+const fs = require('fs');
+const logger = require('../logger');
+const MemoryFileSystem = require('memory-fs');
+const Koa = require('koa');
+const yargs = require('yargs');
 
+let GlobalConfig = {}; //全局设置
+let argvs = yargs
+  .option('d', {
+    alias: 'detail',
+    boolean: true,
+  })
+  .option('dev', {
+    boolean: true,
+  })
+  .option('build', {
+    boolean: true,
+  })
+  .option('port', {
+    default: 8080,
+  }).argv;
 
-// 当前运行目录
-const dir = process.cwd();
-let pathArr = dir.split(path.sep);
-let projectName = pathArr[pathArr.length - 1];
-
-const port = 8080
-let customConfig = {}
-try {
-  customConfig = JSON.parse(fs.readFileSync(path.resolve(dir, "./build.config.json")))
-} catch (err) {
-  customConfig = {}
+//==========================[Main function]============================
+main();
+function main() {
+  if (argvs.d) {
+    return showHelp();
+  }
+  GlobalConfig = loadGlobalConfig(GlobalConfig);
+  if (argvs.dev) {
+    let config = loadWepackConfig('dev');
+    return buildDev(parseInt(argvs.port), config);
+  }
+  if (argvs.build) {
+    let config = loadWepackConfig('prod');
+    return buildPro(config);
+  }
+  console.log('unkonw argument');
 }
-let prodConfig = require('../config/prod.config')
-let devConfig = require('../config/dev.config')
-let config;
-if (argv === '-v') {
-  console.log("cb-cli base on webpack 4")
-} else if (argv === '-dev') {
-  config = devConfig;
-  config.entry = customConfig.entry ? customConfig.entry : config.entry;
-} else if (argv === '-build') {
-  config = prodConfig;
-  config.entry = customConfig.entry ? customConfig.entry : config.entry;
-} else {
-  console.log("unkonw argument")
-  return;
+
+//============================[common]==================================
+function showHelp() {
+  console.log('cb-cli base on webpack4');
 }
 
+function loadGlobalConfig(config) {
+  let projectConfig = {};
+  try {
+    projectConfig = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), './build.config.json')));
+  } catch (err) {
+    console.log('[Error] read project config error');
+  }
+  return Object.assign(config, projectConfig);
+}
 
-const compiler = webpack(config);
-let watch;
-if (argv === '-dev') {
-  stratServer(app);
+function loadWepackConfig(mode) {
+  let config = {};
+  if (mode == 'dev') {
+    config = require('../config/dev.config');
+  } else if (mode == 'prod') {
+    config = require('../config/prod.config');
+    config.output.path = GlobalConfig['OUTPUT_PATH'] ? GlobalConfig['OUTPUT_PATH'] : config.output.path;
+  }
+  config.entry = GlobalConfig['ENTRY_PATH'] ? GlobalConfig['ENTRY_PATH'] : config.entry;
+  return config;
+}
+
+//========================[build develope]=============================
+function buildDev(port, config) {
+  const compiler = webpack(config);
+  const memoryFs = new MemoryFileSystem();
+  const app = new Koa();
   compiler.outputFileSystem = memoryFs; // 输出内存
-  watch = compiler.watch({
-    ignored: `/node_modules/`,
-    aggregateTimeout: 500,
-    poll: 1000
-  }, (err, stats) => {
-    if (err || stats.hasErrors()) {
-      console.log(err || stats.toJson().errors)
-      return
+  stratServer(app, port, memoryFs, config.output.path);
+  let watch = compiler.watch(
+    {
+      ignored: `/node_modules/`,
+      aggregateTimeout: 500,
+      poll: 1000,
+    },
+    (err, stats) => {
+      if (err || stats.hasErrors()) {
+        console.log(`[ERROR] | ${err || stats.toJson().errors}`);
+        return;
+      }
+      let resultJson = stats.toJson().assetsByChunkName;
+      let outputArr = [];
+      Object.keys(resultJson).forEach(key => {
+        if (Array.isArray(resultJson[key])) {
+          outputArr = outputArr.concat(resultJson[key]);
+        } else {
+          outputArr.push(resultJson[key]);
+        }
+      });
+      console.log('[INFO] | Build Success!');
+      outputArr.forEach(item => {
+        console.log(`--- ${item}`);
+      });
+      if (GlobalConfig['HTML_PATH']) {
+        try {
+          handleHTML(GlobalConfig['HTML_PATH'], outputArr);
+        } catch (err) {
+          console.log(`[Error] | Write Error: ${err}`);
+        }
+      }
     }
-    let statsJson = stats.toJson()
-    console.log(`【build】\n${JSON.stringify(statsJson.assetsByChunkName)}`);
-    if (customConfig["HTML_PATH"]) {
-      try {
-        console.log(`handle html... path:${customConfig["HTML_PATH"]}`);
-        handleHTML(customConfig["HTML_PATH"], statsJson.assetsByChunkName); 
-      } catch (err) {}
+  );
+  process.on('SIGINT', function() {
+    watch.close(() => {
+      console.log('[INFO] | Webpack Watching Ended.');
+      process.exit();
+    });
+  });
+}
+
+function stratServer(app, port, memoryFs, basePath) {
+  app.use(async ctx => {
+    try {
+      content = memoryFs.readFileSync(path.resolve(basePath, './' + ctx.path));
+      ctx.type = path.extname(ctx.path);
+      ctx.body = content;
+    } catch (err) {
+      ctx.stats = 404;
+      ctx.body = '404 not found';
     }
   });
-} else if (argv === '-build') {
-  compiler.run((err, stats) => {
-    if (err || stats.hasErrors()) {
-      logger.ciLoger.error(err || stats.toJson().errors)
-      return
-    }
-    let statsJson = stats.toJson()
-    logger.ciLoger.info(`【build】\n${statsJson}`)
-    if (customConfig["HTML_PATH"]) {
-      logger.ciLoger.info(`handle html... path:${customConfig["HTML_PATH"]}`);
-      handleHTMLBuild(customConfig["HTML_PATH"], statsJson.assetsByChunkName);
-    }
+  app.listen(port, () => {
+    console.log('[INFO] | Start develope server at port 8080');
   });
 }
 
 function handleHTML(path, data) {
-  let output = [];
-  Object.keys(data).forEach(key => {
-    if (Array.isArray(data[key])) {
-      output = output.concat(data[key])
-    } else {
-      output.push(data[key])
-    }
-  })
-  let fileData = String(fs.readFileSync(path));
-  console.log("【before】\n", fileData);
-
-  let newFileData = `{{define "resource"}}\n`
-  output.forEach(item => {
+  console.log(`[INFO] | Write resource to html,path:${GlobalConfig['HTML_PATH']}...`);
+  let newFileData = `{{define "resource"}}\n`;
+  data.forEach(item => {
     if (/\.css$/.test(item)) {
-      newFileData += `<link rel="stylesheet" href="//localhost:${port}/${item}">\n`;
+      newFileData += `<link rel="stylesheet" href="//localhost:${argvs.port}/${item}">\n`;
     } else {
-      newFileData += `<script src="//localhost:${port}/${item}"></script>\n`;
+      newFileData += `<script src="//localhost:${argvs.port}/${item}"></script>\n`;
     }
-  })
-  newFileData += "{{end}}"
-  console.log("【after】\n", newFileData);
+  });
+  newFileData += '{{end}}';
   fs.writeFileSync(path, newFileData);
+  console.log('[INFO] | Write resource to html success!');
+}
+
+//========================[build production]===========================
+function buildPro(config) {
+  const compiler = webpack(config);
+  compiler.run((err, stats) => {
+    if (err || stats.hasErrors()) {
+      logger.ciLoger.error(err || stats.toJson().errors);
+      return;
+    }
+    let statsJson = stats.toJson();
+    logger.ciLoger.info(`【build】\n${statsJson}`);
+    if (customConfig['HTML_PATH']) {
+      logger.ciLoger.info(`handle html... path:${customConfig['HTML_PATH']}`);
+      handleHTMLBuild(customConfig['HTML_PATH'], statsJson.assetsByChunkName);
+    }
+  });
 }
 
 function handleHTMLBuild(path, data) {
   let output = [];
   Object.keys(data).forEach(key => {
     if (Array.isArray(data[key])) {
-      output = output.concat(data[key])
+      output = output.concat(data[key]);
     } else {
-      output.push(data[key])
+      output.push(data[key]);
     }
-  })
+  });
   let fileData = String(fs.readFileSync(path));
-  console.log("【before】\n", fileData);
+  console.log('【before】\n', fileData);
 
-  let newFileData = `{{define "resource"}}\n`
+  let newFileData = `{{define "resource"}}\n`;
   output.forEach(item => {
     if (/\.css$/.test(item)) {
       newFileData += `<link rel="stylesheet" href="//106.15.73.155/static/${projectName}/${item}">\n`;
     } else {
       newFileData += `<script src="//106.15.73.155/static/${projectName}/${item}"></script>\n`;
     }
-  })
-  newFileData += "{{end}}"
-  console.log("【after】\n", newFileData);
-  fs.writeFileSync(path, newFileData);
-}
-
-process.on('SIGINT', function () {
-  watch.close(() => {
-    console.log("Webpack Watching Ended.");
-    process.exit()
   });
-});
-
-function stratServer(app){
-  app.use(async (ctx)=>{
-    try {
-      content = memoryFs.readFileSync(path.resolve(devConfig.output.path,ctx.path))
-      ctx.type = path.extname(ctx.path)
-      ctx.body = content; 
-    } catch (err) {
-      ctx.stats = 404;
-      ctx.body = "404 not found";
-    }
-  })
-  app.listen(port, () => {
-    console.log('【dev】 starting at port 8080')
-  })
+  newFileData += '{{end}}';
+  console.log('【after】\n', newFileData);
+  fs.writeFileSync(path, newFileData);
 }
--- config/dev.config.js
+++ config/dev.config.js
@@ -1,133 +1,120 @@
 const path = require('path');
 const webpack = require('webpack');
 // 清除dist插件
-const cleanDist = require('../plugins/cleanDist')
+const cleanDist = require('../plugins/cleanDist');
 // 加载自动化css独立加载插件
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // html模版插件
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 // 当前运行目录
 const dir = process.cwd();
 
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
 let port = 8080;
-let portIndex = process.argv.indexOf('--port');
-if (portIndex !== -1) {
-	process.argv[portIndex + 1] && (port = process.argv[portIndex + 1]);
-}
-
-
 
 //配置正式开始
 module.exports = {
-	//设置入口
-	entry: {
-		app: path.resolve(dir, './app.js'),
-	},
-	//设置打包出口
-	output: {
-		path: "/", //打包文件放在这个目录下
-		filename: '[name].bundle.[hash].js', //打包文件名
-		publicPath: '/',
-	},
-	// source-map
-	devtool: 'inline-source-map',
-	//loader 相关配置
-	module: {
-		rules: [
-			{
-				test: /\.vue$/,
-				use: [
-					{
-						loader: require.resolve('vue-loader'),
-						options: {
-							loaders: {
-								scss: ExtractTextPlugin.extract({
-									fallback: require.resolve('style-loader'),
-									use: [
-										require.resolve('css-loader'),
-										require.resolve('sass-loader'),
-									],
-								}),
-								js: {
-									loader: require.resolve('babel-loader'),
-									options: {
-										babelrc: false,
-										presets: [require.resolve('babel-preset-env'), require.resolve('babel-preset-stage-2')],
-										plugins: [[require.resolve('babel-plugin-transform-runtime'), {
-											"moduleName": path.resolve(__dirname, "../node_modules/babel-runtime")
-										}]],
-									},
-								},
-							},
-						},
-					},
-				],
-			},
-			{
-				test: /\.js$/,
-				exclude: /(node_modules|bower_components)/,
-				use: {
-					loader: require.resolve('babel-loader'),
-					options: {
-						babelrc: false,
-						presets: [require.resolve('babel-preset-react'), require.resolve('babel-preset-env'), require.resolve('babel-preset-stage-2')],
-						plugins: [[require.resolve('babel-plugin-transform-runtime'), {
-							"moduleName": path.resolve(__dirname, "../node_modules/babel-runtime")
-						}]],
-					},
-				},
-			},
-			{
-				test: /\.(scss|css)$/,
-				use: ExtractTextPlugin.extract({
-					fallback: require.resolve('style-loader'),
-					use: [
-						require.resolve('css-loader'),
-						require.resolve('sass-loader'),
-					],
-				}),
-			},
-			{
-				test: /\.(png|svg|jpg|gif)$/,
-				use: {
-					loader: require.resolve('file-loader'),
-					options: {
-						name: 'img/[name].[ext]',
-						publicPath: `//localhost:${port}/`,
-					}
-				},
-			},
-			{
-				test: /\.ejs$/,
-				loader: require.resolve('ejs-loader'),
-			},
-		],
-	},
-	//插件相关配置
-	plugins: [
-		new ExtractTextPlugin({ filename: '[name].bundle.[hash].css', }),
-		new cleanDist(path.resolve(dir, './dist')),
-		new webpack.NamedModulesPlugin(),
-		new webpack.HotModuleReplacementPlugin(),
-	],
-	// 提取第三方库和公共模块
-	optimization: {
-		splitChunks: {
-			cacheGroups: {
-				vendor: {
-					//node_modules内的依赖库
-					chunks: 'all',
-					test: /[\\/]node_modules[\\/]/,
-					name: 'vendor',
-					minChunks: 1, //被不同entry引用次数(import),1次的话没必要提取
-					maxInitialRequests: 5,
-					minSize: 0,
-					priority: 100,
-					// enforce: true?
-				},
-			},
-		},
-	},
-	//设置模式为开发者模式
-	mode: 'development',
+  //设置入口
+  entry: {
+    app: path.resolve(dir, './app.js'),
+  },
+  //设置打包出口
+  output: {
+    path: path.resolve('/dist'), //打包文件放在这个目录下
+    filename: '[name].bundle.[hash].js', //打包文件名
+  },
+  // source-map
+  devtool: 'inline-source-map',
+  //loader 相关配置
+  module: {
+    rules: [{
+        test: /\.vue$/,
+        use: [{
+          loader: require.resolve('vue-loader'),
+        }, ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: require.resolve('babel-loader'),
+          options: {
+            babelrc: false,
+            presets: [
+              require.resolve('babel-preset-react'),
+              require.resolve('babel-preset-env'),
+              require.resolve('babel-preset-stage-2'),
+            ],
+            plugins: [
+              [
+                require.resolve('babel-plugin-transform-runtime'),
+                {
+                  moduleName: path.resolve(__dirname, '../node_modules/babel-runtime'),
+                },
+              ],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          require.resolve('css-loader'),
+          require.resolve('sass-loader'),
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          require.resolve('css-loader')
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif|ttf|woff)$/,
+        use: {
+          loader: require.resolve('file-loader'),
+          options: {
+            name: 'img/[name].[ext]',
+            publicPath: `//localhost:${port}/`,
+          },
+        },
+      },
+      {
+        test: /\.ejs$/,
+        loader: require.resolve('ejs-loader'),
+      },
+    ],
+  },
+  //插件相关配置
+  plugins: [
+    new VueLoaderPlugin(), //vue-loader
+    new MiniCssExtractPlugin({
+      filename: '[name].bundle.[hash].css',
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  // 提取第三方库和公共模块
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          //node_modules内的依赖库
+          chunks: 'all',
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          minChunks: 1, //被不同entry引用次数(import),1次的话没必要提取
+          maxInitialRequests: 5,
+          minSize: 0,
+          priority: 100,
+          // enforce: true?
+        },
+      },
+    },
+  },
+  //设置模式为开发者模式
+  mode: 'development',
 };
